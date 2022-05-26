06a7627ffab8f8f5a2f076d7d0a01dd682c7d733
johnny310 <janagiraman31.in@gmail.com>
2022-05-26 10:04:17 +0530

    mini project

--- src/app/default/default.component.ts
+++ src/app/default/default.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-default',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefaultComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
+
+logout(){
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, Logout it!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      this.router.navigateByUrl('login');
+      Swal.fire(
+        'Logout Sucessfully!',
+        'Thank You 🤗️',
+        'success'
+      )
+    }
+  })
+}
 
 }
